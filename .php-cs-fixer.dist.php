b7142b4132822b0fc049a0097ca2d6a9e4ec4e33
Rob Woodward <robwdwd@icloud.com>
2021-06-29 11:35:29 +0000

    Update php-cs and phpmd rules

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,27 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('vendor')
+    ->in(__DIR__)
+;
+
+$config = new PhpCsFixer\Config();
+
+return $config->setRules([
+    '@PSR2' => true,
+    '@PSR12' => true,
+    '@Symfony' => true,
+    'array_indentation' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
+    'no_useless_else' => true,
+    'no_useless_return' => true,
+    'phpdoc_add_missing_param_annotation' => ['only_untyped' => false],
+    'no_superfluous_phpdoc_tags' => false,
+    'phpdoc_no_empty_return' => true,
+    'phpdoc_order' => true,
+    'phpdoc_separation' => true,
+    'phpdoc_var_annotation_correct_order' => true,
+])
+    ->setFinder($finder)
+;
