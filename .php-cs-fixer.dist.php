195056e181a6ee4dae5eeff13c2a91d38db0f841
Rob Woodward <rob@emailplus.org>
2024-02-21 09:07:35 +0000

    Update rector

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,6 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()->in(__DIR__.'/src');
+$finder = PhpCsFixer\Finder::create()->in(__DIR__)->exclude('vendor');
 
 $config = new PhpCsFixer\Config();
 
--- rector.php
+++ rector.php
@@ -3,14 +3,16 @@
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withSymfonyContainerXml(__DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml')
-    ->withPhpSets()
-    ->withPreparedSets(codeQuality: true, codingStyle: true)
-    ->withAttributesSets(symfony: true, doctrine: true)
-    ->withPaths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ])
+        ->withSymfonyContainerXml(__DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml')
+        ->withPhpSets()
+        ->withPreparedSets(codeQuality: true, codingStyle: true)
+        ->withAttributesSets(symfony: true, doctrine: true)
+        ->withSkip([
+            __DIR__ . '/vendor',
+        ])
+        ->withPaths([
+            __DIR__ 
+        ])
     ;
-    
+
 
